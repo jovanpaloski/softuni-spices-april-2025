f57a7e804af0d7a2d0b92b3b3b8fecbb53d342f2
jovan <jovanpaloski@gmail.com>
2025-04-04 21:33:46 +0200

    Get all spices

---
+++ client/src/services/spiceService.js
@@ -0,0 +1,17 @@
+import request from "../utils/request.js";
+
+
+const baseUrl = 'http://localhost:3030/jsonstore/spices';
+
+export default {
+    async getAll() {
+        const result = await request.get(baseUrl);
+
+        const spices = Object.values(result);
+
+        return spices;
+    },
+    create(spiceData) {
+        return request.post(baseUrl, spiceData);
+    }
+}
