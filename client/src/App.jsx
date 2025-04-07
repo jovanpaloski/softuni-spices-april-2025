ee8c24471ca1a2b364da4ae473fbb759c3a11a23
jovan <jovanpaloski@gmail.com>
2025-04-07 19:22:07 +0200

    Add react router with home route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,3 +1,4 @@
+import { Routes, Route } from 'react-router'
 import './App.css'
 import Header from './components/header/Header'
 import Home from './components/home/Home'
@@ -8,7 +9,9 @@ function App() {
       <Header />
 
       <main id="main-content">
-        <Home />
+        <Routes>
+          <Route index element={<Home />} />
+        </Routes>
       </main>
     </div>
   )
